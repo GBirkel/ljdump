b2e2b01d01fd292e1f3c5bde699647f29b2187ff
Garrett Birkel <gbirkel@me.com>
2024-04-02 19:03:52 -0700

    Version bump

--- ljdump.py
+++ ljdump.py
@@ -3,7 +3,7 @@
 #
 # ljdump.py - livejournal archiver
 # Greg Hewgill, Garrett Birkel, et al
-# Version 1.7.1
+# Version 1.7.2
 #
 # LICENSE
 #
--- ljdumpsqlite.py
+++ ljdumpsqlite.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # ljdumpsqlite.py - SQLite support tools for livejournal archiver
-# Version 1.7.1
+# Version 1.7.2
 #
 # LICENSE
 #
